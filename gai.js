72c2715cb2256327eab8c8f7e085c333f9f7d46f
khuongkia <82940331+khuongkia@users.noreply.github.com>
2021-04-21 16:43:49 +0700

    Update gai.js

--- gai.js
+++ gai.js
@@ -1,1 +1,39 @@
 
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+$url = file_get_contents('https://api.tumblr.com/v2/blog/gaixinhchonloc.com/posts/photo?api_key=6L7Cy5WSd2wRrretr6nzRy0ymhOmS0tr43PbEUJeQ649LRDBif');
+$json = json_decode($url, true);
+$json_posts = $json['response']['posts'];
+$data = array();
+foreach ($json_posts as $key => $value) {
+  $data[] = $value['photos'][0]['original_size']['url'];
+}
+function array_random($array, $amount = 1){
+   $keys = array_rand($array, $amount);
+
+   if ($amount == 1) {
+       return $array[$keys];
+   }
+
+   $results = [];
+   foreach ($keys as $key) {
+       $results[] = $array[$key];
+   }
+
+   return $results;
+}
+$link_img = array_random($data);
+$result = array(
+	'messages' => array(
+		'0' => array(
+			'attachment' => array(
+				'type' => 'image',
+				'payload' => array(
+					'url' => $link_img
+				)
+			)
+		),
+	)
+);
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
