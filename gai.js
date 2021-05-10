4702f0b8926d390cb55924b0c8e5c20ce1987ebb
khuongkia <82940331+khuongkia@users.noreply.github.com>
2021-05-10 18:32:13 +0700

    Update gai.js

--- gai.js
+++ gai.js
@@ -1,1 +1,21 @@
-module.exports.config = { 	name: "restart", 	version: "0.0.1-beta", 	hasPermssion: 2, 	credits: "Thanh Dz", 	description: "", 	commandCategory: "system", 	usages: "", 	dependencies: ["fs-extra","fs","merge-images","path","wait-on","canvas","node-cmd"], 	cooldowns: 0, 	envConfig: { 		unsendMessageAfter: 5 	} }; module.exports.run = async function ({ event, api, client, utils, Currencies, Threads }) { let {messageID, senderID, threadID, body } = event; return api.sendMessage("Bot sẽ khởi động lại ngay 🥀", threadID, () => require("node-cmd").run("pm2 restart 0"), messageID); 	}
+module.exports.config = {
+	name: "calladmin",
+	version: "1.0.0", 
+	hasPermssion: 0,
+	credits: "HungCatMoi",
+	description: "Gửi tin nhắn đến Admin",
+	commandCategory: "System", 
+	usages: "calladmin [Tin nhắn]", 
+	cooldowns: 0,
+	dependencies: [] 
+};
+
+module.exports.run = async function({ api, event, args, Currencies, utils, Users,__GLOBAL }) {
+  var content = args.join(" ")
+  let name = (await api.getUserInfo(event.senderID))[event.senderID].name;
+  let url = (await api.getUserInfo(event.senderID))[event.senderID].profileUrl;
+ const moment = require("moment");
+var time = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss L");
+ if (!content) api.sendMessage("Bạn chưa nhập tin nhắn 💩💩", event.threadID, event.messageID)
+  else api.sendMessage(🤡Người Dùng Bot tên: ${name} đã gửi 1 lời dắn đến Admin !\n📱URL: ${url}\n⌚️Thời Gian: ${time}\n🔈Nội dung: ${content} 	__GLOBAL.settings.ADMINBOT, () => api.sendMessage("Okey tin nhắn của bạn đã được gửi tới Admin ❤️❤️", event.threadID, event.messageID))
+}
