a0b3968eca91502c79a1482374ccd3edfc0ec89d
khuongkia <82940331+khuongkia@users.noreply.github.com>
2021-05-14 18:26:14 +0700

    out.js

--- calladmin.js
+++ calladmin.js
@@ -1,21 +1,19 @@
 module.exports.config = {
-	name: "calladmin",
-	version: "1.0.0", 
-	hasPermssion: 0,
-	credits: "HungCatMoi",
-	description: "Gửi tin nhắn đến Admin",
-	commandCategory: "System", 
-	usages: "calladmin [Tin nhắn]", 
-	cooldowns: 0,
-	dependencies: [] 
+  name: "out",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "VanHung & fix by Dung UwU",
+  description: "Out theo id box",
+  commandCategory: "Admin",
+  usages: "out",
+  cooldowns: 3
 };
 
-module.exports.run = async function({ api, event, args, Currencies, utils, Users,__GLOBAL }) {
-  var content = args.join(" ")
-  let name = (await api.getUserInfo(event.senderID))[event.senderID].name;
-  let url = (await api.getUserInfo(event.senderID))[event.senderID].profileUrl;
- const moment = require("moment");
-var time = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss L");
- if (!content) api.sendMessage("Bạn chưa nhập tin nhắn 💩💩", event.threadID, event.messageID)
-  else api.sendMessage(🤡Người Dùng Bot tên: ${name} đã gửi 1 lời dắn đến Admin !\n📱URL: ${url}\n⌚️Thời Gian: ${time}\n🔈Nội dung: ${content} 	__GLOBAL.settings.ADMINBOT, () => api.sendMessage("Okey tin nhắn của bạn đã được gửi tới Admin ❤️❤️", event.threadID, event.messageID))
+module.exports.run = async function({ api, event, args }) {
+	const moment = require("moment-timezone");
+	var time = moment.tz('Asia/Ho_Chi_Minh').format('HH:mm:ss || DD/MM/YYYY');
+    const name = args.join(" ")
+   let namee = await api.getThreadInfo(name)
+  if (!name) api.removeUserFromGroup(api.getCurrentUserID(), event.threadID)
+else api.sendMessage(`Bot đã nhận lệnh rời khỏi nhóm ${namee.name} từ Admin\nVào lúc: ${time}`, name, () => api.removeUserFromGroup(api.getCurrentUserID(), name)).then(api.sendMessage("💦Bot đã rời nhóm " + namee.name, event.threadID, event.messageID))
 }
