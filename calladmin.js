1acb0d916d2ffd6bae76de684549e6856abd8079
khuongkia <82940331+khuongkia@users.noreply.github.com>
2021-05-17 19:56:16 +0700

    Update calladmin.js

--- calladmin.js
+++ calladmin.js
@@ -1,18 +1,40 @@
 module.exports.config = {
-	name: "cadao",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "CatalizCS",
-	description: "Chưa có",
-	commandCategory: "other",
-	usages: "rname",
-	cooldowns: 5,
-	dependencies: ["request"],}
+        name: "calladmin",
+        version: "1.0.0",
+        hasPermssion: 0,
+        credits: "Thọ",
+        description: "gửi tin nhắn tới admin của bot",
+        commandCategory: "group",
+        usages: "calladmin [lời nhắn]",
+        cooldowns: 30,
+        dependencies :[]
 };
 
-module.exports.run = async ({ api, event, __GLOBAL }) => {
-	return require("request")(`https://api.berver.tech/cadao`, (err, response, body) => {
-		const data = JSON.parse(body);
-		api.sendMessage({body: `${content.data}`, event.threadID, event.senderID);
-	});
+module.exports.handleReply = function({ api, event, args, handleReply, client, __GLOBAL }) {
+     switch (handleReply.type) {
+        case "reply": {
+          api.sendMessage(`Phản hồi từ admin:\n${event.body}`,handleReply.threadID,handleReply.messageID)
+       break;
+        }
+          default:
+             break;
+   }
 }
+
+module.exports.run = async function({ api, event, args, Currencies, utils, Users,Threads,__GLOBAL,client}) {
+  let {senderID, messageID, threadID} = event
+ if (!args[0]) api.sendMessage("Bạn chưa nhập tin nhắn !", threadID,messageID);
+   let data = await Users.getData(senderID)
+   let thread = await Threads.getData(threadID)
+for(var id of __GLOBAL.settings.ADMINBOT){
+  api.sendMessage(`[${data.name} - ${senderID}] tại nhóm ${thread.name} nhắn: ${args.join(" ")}`,id)
+  await new Promise(resolve => setTimeout(resolve, 1000));
+};
+  return api.sendMessage("Thông tin của bạn đã được gửi tới các admin",threadID,messageID, (error, info) => {
+                        client.handleReply.push({
+                            messageID: info.messageID,
+                            threadID,
+                            type: "reply"
+                        })
+    })
+  }
