9530ee84377eed4f07c76c44bc98e33543c48d6a
khuongkia <82940331+khuongkia@users.noreply.github.com>
2021-05-22 21:12:56 +0700

    ?.js

--- trongcay.js
+++ trongcay.js
@@ -1,53 +1,24 @@
-
+const fs = require("fs");
 module.exports.config = {
-	name: "trongcay",
-	version: "0.0.1",
+name: "test1",
+	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "CatalizCS",
-	description: "Có làm thì mới có ăn!",
-	commandCategory: "Economy",
-	usages: "work",
-    cooldowns: 5,
-    envConfig: {
-        cooldownTime: 1200000
-    }
+	credits: "VanHung (thĂªm random by KhÆ°Æ¡ng)",
+	description: "hi",
+	commandCategory: "KhĂ´ng cáº§n dáº¥u lá»‡nh",
+	usages: "noprefix",
+	cooldowns: 5,
 };
+module.exports.event = function({ api, event }) {
 
-module.exports.run = async ({ event, api, Currencies, __GLOBAL }) => {
-    const { threadID, messageID } = event;
-    const cooldown = __GLOBAL.work.cooldownTime;
-    const data = (await Currencies.getData(event.senderID)).workTime;
-    if (typeof data !== "undefined" && cooldown - (Date.now() - data) > 0) {
-        var time = cooldown - (Date.now() - data),
-            minutes = Math.floor(time / 60000),
-            seconds = ((time % 60000) / 1000).toFixed(0);
-        
-		return api.sendMessage(`Ruộng của bạn đang trong thời gian chờ\nVui lòng thử lại sau: ${minutes} phút ${(seconds < 10 ? "0" : "")}${seconds} giây!`, event.threadID, event.messageID);
-    }
-    else {
-        const job = [
-            "khế",
-            "dưa hấu",
-            "dưa leo",
-            "bí đao",
-            "củ cải",
-            "chuối",
-            "đu đủ",
-            "chanh",
-            "xoài",
-            "bưởi",
-            "cam",
-            "chôm chôm",
-            "cóc",
-            "chùm ruột",
-            "dừa",
-            "mãng cầu"
-        ];
-        const amount = Math.floor(Math.random() * 600);
-        return api.sendMessage(`Bạn vừa thu hoạch cây ${job[Math.floor(Math.random() * job.length)]} và bán được số tiền là: ${amount} coins`, threadID, async () => {
-             await Currencies.increaseMoney(event.senderID, parseInt(amount));
-             await Currencies.setData(event.senderID, { workTime: Date.now() });
-        }, messageID);
-    }
-       
-}
+var { threadID, messageID } = event;
+	if (event.body.indexOf("test 1")==0 ||(event.body.indexOf("Test 1")==0)) {
+
+	var msg = {attcahment: __dirname + ""};
+  
+			return api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	}
