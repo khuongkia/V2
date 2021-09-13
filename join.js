d4286d621cbb2314f0af10d655c704056668cc60
khuongkia <82940331+khuongkia@users.noreply.github.com>
2021-09-13 17:31:45 +0700

    Update join.js

--- join.js
+++ join.js
@@ -1,53 +1,45 @@
 module.exports.config = {
-	name: "join",
-	eventType: ["log:subscribe"],
-	version: "1.0.1",
-	credits: "Mirai Team",
-	description: "Thông báo bot hoặc người vào nhóm",
+	name: "tikvideo",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "KhánhMilo",
+	description: "Tải video tik tok xóa logo",
+	commandCategory: "media",
+	usages: "[url]",
+	cooldowns: 10,
 	dependencies: {
+		"axios": "",
 		"fs-extra": ""
 	}
+	
 };
+module.exports.run = async function({ api, event, args,}) {
+  const axios = global.nodemodule["axios"];
+  const fs = global.nodemodule["fs-extra"];
+  if (!args.join("") != " " ){ return api.sendMessage("Bạn phải ngập url video tiktok !!!", event.threadID, event.messageID);}
+  var uid = args[0];
+  try {
+	const options = {
+		method: 'GET',
+		url: 'https://video-nwm.p.rapidapi.com/url/',
+		params: {url: `\'${uid}\'`},
+		headers: {
+		  'x-rapidapi-key': '2082412fd5msh902a44c9a8ab843p15d921jsn82bc78439711',
+		  'x-rapidapi-host': 'video-nwm.p.rapidapi.com'
+		}
+	  };
+	  var data = await axios.request(options);
+	  if(data.data.item.video.playAddr[0] == undefined ) get = data.data.item.video.playAddr
+	  else get = data.data.item.video.playAddr[0]
+	  pathus = __dirname+`/cache/${event.threadID}-${event.senderID}.mp4`  
+	  var getms = (await axios.get(get,{responseType: "arraybuffer"})).data; 
+	  fs.writeFileSync(pathus, Buffer.from(getms, "utf-8"));
+	  console.log(get);
+	  if (fs.statSync(__dirname + `/cache/${event.threadID}-${event.senderID}.mp4`).size > 26214400) return api.sendMessage('Không thể gửi file vì dung lượng lớn hơn 25MB.', event.threadID, () => unlinkSync(__dirname + `/cache/${event.threadID}-${event.senderID}.mp4`), event.messageID);
+	  else return api.sendMessage({body : "Loading success ✅" , attachment: fs.createReadStream(__dirname + `/cache/${event.threadID}-${event.senderID}.mp4`)}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/${event.threadID}-${event.senderID}.mp4`), event.messageID)
+	 
+		}catch {
+			return api.sendMessage('Không thể xử lý yêu cầu của bạn! Hãy thử xóa / ở cuối link xem', event.threadID, event.messageID);
+		};
+ }
 
-module.exports.run = async function({ api, event }) {
-	const { join } = global.nodemodule["path"];
-	const { threadID } = event;
-	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-		api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "Made by CatalizCS and SpermLord" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
-		return api.sendMessage(`Connected successfully! This bot was made by CatalizCS and SpermLord\nThank you for using our products, have fun UwU <3`, threadID);
-	}
-	else {
-		try {
-			const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-			let { threadName, participantIDs } = await api.getThreadInfo(threadID);
-
-			const threadData = global.data.threadData.get(parseInt(threadID)) || {};
-			const path = join(__dirname, "cache", "joinGif");
-			const pathGif = join(path, `1.gif`);
-
-			var mentions = [], nameArray = [], memLength = [], i = 0;
-			
-			for (id in event.logMessageData.addedParticipants) {
-				const userName = event.logMessageData.addedParticipants[id].fullName;
-				nameArray.push(userName);
-				mentions.push({ tag: userName, id });
-				memLength.push(participantIDs.length - i++);
-			}
-			memLength.sort((a, b) => a - b);
-			
-			(typeof threadData.customJoin == "undefined") ? msg = "Welcome aboard {name}.\nChào mừng đã đến với {threadName}.\n{type} là thành viên thứ {soThanhVien} của nhóm 🥳" : msg = threadData.customJoin;
-			msg = msg
-			.replace(/\{name}/g, nameArray.join(', '))
-			.replace(/\{type}/g, (memLength.length > 1) ?  'các bạn' : 'bạn')
-			.replace(/\{soThanhVien}/g, memLength.join(', '))
-			.replace(/\{threadName}/g, threadName);
-
-			if (existsSync(path)) mkdirSync(path, { recursive: true });
-
-			if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
-			else formPush = { body: msg, mentions }
-
-			return api.sendMessage(formPush, threadID);
-		} catch (e) { return console.log(e) };
-	}
-}
